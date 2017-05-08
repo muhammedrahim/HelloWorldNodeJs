6df872f13e46890949a4105e4b9a04d6b153a44d
muhammedrahim <muhammed.rahim@sasol.com>
2017-05-08 09:40:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello New World!');
+    res.send('Hello Gerhard');
 });
 
 var server = app.listen(port, function () {
